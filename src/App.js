e19e0d0fa5311eb82f9fdf0b109d976bd1ec17b5
seanthenderson <seanthenderson@gmail.com>
2018-01-16 12:48:26 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import Items from './components/Items.js';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Items />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Items.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Items extends Component() {
+    render() {
+        return(
+           <div>Item</div> 
+        ); 
+    }
+}
+
+export default Items;
